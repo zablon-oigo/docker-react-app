872111d2e7eea5fae13d601f97c2caeb191a0792
zablon <zablonoigo51@gmail.com>
2024-01-09 16:01:35 +0300

    project route path configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { BrowserRouter, Outlet,Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import NavabarLayout from "./Layout/NavabarLayout"
+import Home from "./components/Home"
 
 
 function App() {
   return (
     <>
-<h2 className="text-3xl font-bold underline text-pink-700">
-  React + Doker + Tailwind
-</h2>
- 
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<NavabarLayout/>} >
+
+        <Route index element={<Home/>} />
+        <Route path="about" element={<h1>About</h1>} />
+        <Route path="contact" element={<h1>Contact</h1>} />
+        <Route path="services" element={<h1>services</h1>} />
+
+        </Route>
+    </Routes>
+    </BrowserRouter>
+
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['Quicksand', 'sans-serif'],
+    }
   },
   plugins: [],
 }
