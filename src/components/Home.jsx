980dbc042c9f9128f41bf362fd270bef15437b5b
zablon <zablonoigo51@gmail.com>
2024-01-10 11:38:32 +0300

    compone parent file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,13 @@
 import React from 'react'
-import header from '../assets/images/header.jpg'
-import Button from '../Layout/Button'
+import Header from './home/Header'
+import About from './home/About'
 function Home() {
-    const bgImage={
-        backgroundImage:`url(${header})`,
-        height:'100vh',
-        backgroundSize:'cover',
-        backgroundPosition:'center',
-        backgroundRepeat:'no-repeat'
 
-    }
   return (
     <>
-    <div className="absolute top-0 right-0 w-full -z-10" style={bgImage}>
-           <div className="md:px-20 px-6 md:py-80 py-60 ">
-           <div className="md:w-2/3 w-full">
-          <p className="text-2xl text-center md:text-start text-white font-bold">Pet Care Services, in New York City</p>
-          <h2 className="md:text-8xl text-4xl mt-6 md:mt-4 md:text-start text-center  font-extrabold text-white ">
-          We Provide Care, That Your Pet Deserves!
-          </h2>
-          <div className="mt-4 flex justify-center md:justify-start">
-            <Button title="Get Started"/>
-          </div>
-        </div>
-           </div>
-    </div>
+    <Header/>
+    <About/>
+   
 
     </>
   )
