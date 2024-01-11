b4c62191f0dc5939c75cf27e12eaaba4513aad6d
zablon <zablonoigo51@gmail.com>
2024-01-11 14:21:22 +0300

    New Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import Header from './home/Header'
 import About from './home/About'
 import Location from './home/Location'
 import Reviews from './home/Reviews'
+import Services from './home/Services'
 function Home() {
 
   return (
@@ -11,6 +12,7 @@ function Home() {
     <About/>
     <Location/>
     <Reviews/>
+    <Services/>
    
 
     </>
