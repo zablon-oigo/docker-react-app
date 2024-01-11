2ee3b9fac8ef50a7c22419083f2243d08ca64c2b
zablon <zablonoigo51@gmail.com>
2024-01-11 22:03:41 +0300

    Testimonial Component

---
+++ src/components/home/Testimonial.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Testimonial = () => {
+  return (
+    <>
+    <div className="md:px-20 px-6 py-10 bg-gray-100 min-h-[60vh] bg-gradient-to-br from-amber-100 to-yellow-50">
+        <h2 className="text-3xl text-center font-white">testimonial</h2>
+        <h2 className="md:text-6xl text-3xl text-center  font-extrabold text-amber-500 uppercase  ">happy faces</h2>
+        <p className="font-light md:text-xl text-lg text-center  mb-2 leading-tight">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum dolorum suscipit alias, reiciendis qui assumenda totam. Inventore quo fugiat sapiente?
+        </p>
+    </div>
+    </>
+  )
+}
+
+export default Testimonial
