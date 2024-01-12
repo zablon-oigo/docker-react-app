ea377385ca99027612081bafce308c8ada9d3e7c
zablon <zablonoigo51@gmail.com>
2024-01-12 09:25:36 +0300

    Testimonial Card

---
+++ src/Layout/TestimonialCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function TestimonialCard() {
+    const card=[
+        {
+
+        },
+    ]
+  return (
+    <>
+    <div className="">
+        {card.map((item)=>{
+            return(
+                <div className="" key={item}>
+                    
+                </div>
+            )
+        })}
+    </div>
+    </>
+  )
+}
+
+export default TestimonialCard
