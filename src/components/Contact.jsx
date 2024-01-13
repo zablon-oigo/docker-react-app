f3230c788cdb2ecbd93973a5e0093c21035e5017
zablon <zablonoigo51@gmail.com>
2024-01-13 12:34:47 +0300

    Adding export function for deployment

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+      <h2 className="text-3xl">
+        Contact Page Not yet complete
+      </h2>
+    </div>
+  )
+}
+
