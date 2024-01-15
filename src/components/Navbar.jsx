044d725d92f27fe764be6a2f6c29699420b06f7f
zablon <zablonoigo51@gmail.com>
2024-01-15 16:52:58 +0300

    Navbar Styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,13 +6,26 @@ function Navbar() {
   return (
     <>
     <div className="flex items-center  space-x-6">
-            <Link to="." className="text-xl font-medium text-white">Home</Link>
-            <Link to="about" className="text-xl font-medium text-white">Who We Are</Link>
-            <Link to="contact" className="text-xl font-medium text-white">Contact</Link>
-            <Link to="services" className="text-xl font-medium text-white">Services</Link>
+            <Link to="." className="duration-300 text-xl font-medium text-white relative group">
+              Home
+              <span className="duration-300 inset-x-0 absolute bottom-0  h-0.5 bg-amber-500 origin-bottom scale-x-0 group-hover:scale-x-100 transition-transform "></span>
+              </Link>
+            <Link to="about" className="text-xl font-medium text-white relative group duration-300">
+              Who We Are
+              <span className="duration-300 inset-x-0 absolute bottom-0  h-0.5 bg-amber-500 origin-bottom scale-x-0 group-hover:scale-x-100 transition-transform "></span>
+              </Link>
+              <Link to="services" className="text-xl font-medium text-white relative group duration-300">
+              Services
+              <span className="duration-300 inset-x-0 absolute bottom-0  h-0.5 bg-amber-500 origin-bottom scale-x-0 group-hover:scale-x-100 transition-transform "></span>
+              </Link>
+            <Link to="contact" className="text-xl font-medium text-white relative group duration-300">
+              Contact
+              <span className="duration-300 inset-x-0 absolute bottom-0  h-0.5 bg-amber-500 origin-bottom scale-x-0 group-hover:scale-x-100 transition-transform "></span>
+              </Link>
+            
             <div className="mx-2">
-       <Button title="Download APP"/>
-       </div> 
+                <Button title="Download APP"/>
+            </div> 
        </div>
 
     </>
